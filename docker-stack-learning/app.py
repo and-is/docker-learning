b7d9e2e5042cd36759f57e7159bd900ed488ab07
Andis Paudel <andispaudel@gmail.com>
2024-12-04 22:39:36 +0545

    made correction to typos

--- docker-stack-learning/app.py
+++ docker-stack-learning/app.py
@@ -3,9 +3,9 @@ from redis import Redis,RedisError
 import os
 import socket
 
-redis = Redis(host="redis", db=0, socker_connect_timeout=2, socket_timeout=2)
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
-app = Flash(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 
@@ -18,7 +18,7 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname: </b> {hostname}<br/>" \
            "<b>Visits: </b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socker.gethostname(), visit=visits)
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80) 
